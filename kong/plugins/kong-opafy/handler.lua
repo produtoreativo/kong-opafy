3290c89176ba312432177e3f35a6d78e007d925c
VitoorFranca <franc.vitoor@gmail.com>
2025-12-12 11:21:42 -0300

    debug

--- kong/plugins/kong-opafy/handler.lua
+++ kong/plugins/kong-opafy/handler.lua
@@ -10,26 +10,20 @@ function KongOPAfy:access(config)
   -- monta o input para o OPA
   local input = {}
 
-  if config.include_headers then
-    input.headers = kong.request.get_headers()
-  end
+  input.headers = kong.request.get_headers()
 
-  if config.include_query then
-    input.query = kong.request.get_query()
-  end
+  input.query = kong.request.get_query()
 
-  if config.include_body then
-    local body_raw = kong.request.get_raw_body()
+  local body_raw = kong.request.get_raw_body()
 
-    if body_raw then
-      -- tenta decodificar o JSON
-      local ok, decoded = pcall(cjson.decode, body_raw)
+  if body_raw then
+    -- tenta decodificar o JSON
+    local ok, decoded = pcall(cjson.decode, body_raw)
 
-      if ok then
-        input.body = decoded
-      else
-        input.body = body_raw  -- fallback
-      end
+    if ok then
+      input.body = decoded
+    else
+      input.body = body_raw  -- fallback
     end
   end
 
@@ -39,13 +33,7 @@ function KongOPAfy:access(config)
 
   local url = config.opa_url .. "/v1/data/authz/allow"
 
-  print("============================ OPA INPUT PAYLOAD 1 ==================================")
-
   kong.log.inspect(input, "OPA INPUT PAYLOAD 2")
-  kong.log.inspect(cjson.encode({ input = input }), "OPA INPUT PAYLOAD 3")
-
-
-  print("============================ OPA INPUT PAYLOAD 1 ==================================")
 
   local res, err = httpc:request_uri(url, {
     method = "POST",
